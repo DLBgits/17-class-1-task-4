edda0a1f0bacb3b2c20e2713387cd87b8c54c0b1
forrestrun7 <569291459@qq.com>
2019-11-12 21:10:04 +0800

    update

---
+++ (1705010116)汪昊-(1705010114)王亚轩-(1701020109)陈殿坤/src/tkedit.py
@@ -0,0 +1,18 @@
+from texteditor import main
+import os
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox as msg
+from pathlib import Path
+
+import yaml
+
+from tkinter import filedialog
+
+from textarea import TextArea
+from linenumbers import LineNumbers
+from highlighter import Highlighter
+from findwindow import FindWindow
+from colourchooser import ColourChooser
+from fontchooser import FontChooser
+main()
